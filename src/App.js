8ead4a774e51438b4152771629d69766e5cd6b7f
JonesTPG <joonas.ryynanen1@gmail.com>
2018-08-24 19:13:15 +0300

    Lähdetään tekemään 2.6.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,33 @@
-import React from 'react'
-import Kurssi from './components/Kurssi'
-import Otsikko from './components/Otsikko'
-import Sisalto from './components/Sisalto'
-import Osa from './components/Osa'
+import React from 'react';
 
-
-
-
-
-
-const App = () => {
-
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
     }
-  ]
+  }
 
-
-  return (
-    <div>
-      <h1> Opetusohjelma (Branch-testi) </h1>
-      {kurssit.map(yksiKurssi=><Kurssi key={yksiKurssi.id} kurssi={yksiKurssi}/>)}
-     
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form>
+          <div>
+            nimi: <input />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        ...
+      </div>
+    )
+  }
 }
 
 export default App
