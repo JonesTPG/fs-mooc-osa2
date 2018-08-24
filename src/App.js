9875c6bf2b09f3b7604e5dc8112d11a0fa377489
JonesTPG <joonas.ryynanen1@gmail.com>
2018-08-24 20:47:49 +0300

    2.8 tehty!

--- src/App.js
+++ src/App.js
@@ -5,30 +5,35 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas',
+          number: '112' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
-  addNumber = (event) => {
+  addItem = (event) => {
     event.preventDefault()
     
     let nimiLista = this.state.persons.map(rivi => rivi.name)
     if ( nimiLista.includes(this.state.newName) ) {
       alert("Nimi on jo listassa!")
-      this.setState({newName: ''})
+      this.setState({newName: '',
+                     newNumber: ''})
       return
     }
 
     const nameObject = {
       name: this.state.newName,
+      number: this.state.newNumber
     }
     const persons = this.state.persons.concat(nameObject)
   
     this.setState({
       persons: persons,
-      newName: ''
+      newName: '',
+      newNumber: ''
     })
   }
 
@@ -36,21 +41,28 @@ class App extends React.Component {
     this.setState({ newName: event.target.value })
   }
 
+  handeNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addNumber}>
+        <form onSubmit={this.addItem}>
           <div>
             nimi: <input value={this.state.newName}
                           onChange={this.handeNameChange} />
+            <br/>
+            numero: <input value={this.state.newNumber}
+                          onChange={this.handeNumberChange} />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(rivi =>  <p key={rivi.name}> {rivi.name} <br/></p>)}
+        {this.state.persons.map(rivi =>  <p key={rivi.name}> {rivi.name} {rivi.number}<br/></p>)}
       </div>
     )
   }
