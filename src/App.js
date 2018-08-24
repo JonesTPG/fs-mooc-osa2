eae4b9105f15c3e91631c5bbbcbdac2090f64ec3
JonesTPG <joonas.ryynanen1@gmail.com>
2018-08-24 20:10:43 +0300

    2.6 tehty!

--- src/App.js
+++ src/App.js
@@ -11,20 +11,43 @@ class App extends React.Component {
     }
   }
 
+  addNumber = (event) => {
+    event.preventDefault()
+    
+    const nameObject = {
+      name: this.state.newName,
+    }
+    const persons = this.state.persons.concat(nameObject)
+  
+    this.setState({
+      persons: persons,
+      newName: ''
+    })
+  }
+
+  handeNameChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
   render() {
     return (
       <div>
+        <div>
+          debug: {this.state.newName}
+        </div>
         <h2>Puhelinluettelo</h2>
-        <form>
+        <form onSubmit={this.addNumber}>
           <div>
-            nimi: <input />
+            nimi: <input value={this.state.newName}
+                          onChange={this.handeNameChange} />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        ...
+        {this.state.persons.map(rivi =>  <p key={rivi.name}> {rivi.name} <br/></p>)}
       </div>
     )
   }
