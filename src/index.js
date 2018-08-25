ad3fa9d924f72e4cf16a939a98f5d0772474a704
JonesTPG <joonas.ryynanen1@gmail.com>
2018-08-25 18:19:51 +0300

    teoriaa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import React from 'react'
 import App from './App'
-import axios from 'axios'
 
 
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  )
-
-
-
-
-  
-
+ReactDOM.render(
+  <App  />,
+  document.getElementById('root')
+)
